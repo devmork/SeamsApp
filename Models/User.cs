577eadbb53cda992710eb1c6ac45a60bebcbf922
devmork <enoymarkden@gmail.com>
2025-10-09 19:23:28 +0800

    feat: add new Role property on User class

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeamsApp.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string? UserName { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        public string? Role { get; set; }
+    }
+}
