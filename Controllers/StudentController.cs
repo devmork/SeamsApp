25211fac082530b779cb2b49c798479dda57c0e6
devmork <enoymarkden@gmail.com>
2025-12-27 22:32:08 +0800

    refactor: remove unused methods and repository dependency from StudentController

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -10,57 +10,7 @@ namespace SeamsApp.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
-        private readonly IStudentsRepository _studentRepository;
-        public StudentController(IStudentsRepository studentsRepository)
-        {
-            _studentRepository = studentsRepository;
-        }
-        [HttpGet]
-        public async Task<ActionResult<List<Student>>> GetAllStudents()
-        {
-            var students = await _studentRepository.GetAllStudent();
-            if (students == null)
-            {
-                return BadRequest();
-            }
-            return Ok(students);
-        }
         
-        [HttpPost]
-        public async Task<ActionResult<int>> AddStudent([FromBody] Student student)
-        {
-            return Ok(await _studentRepository.AddStudent(student));
-        }
-
-        [HttpPut("{id}")]
-        public void UpdateStudent(int id, [FromBody] Student student)
-        {
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<Student>> GetStudentByID(string schoolStudentID )
-        {
-            var student = await _studentRepository.GetStudentById(schoolStudentID);
-
-            if (student == null)
-            {
-                return BadRequest();
-            }
-
-            return Ok(student);
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<Student>> GetStudentQRCode(string schoolStudentID)
-        {
-            var qrcode = await _studentRepository.GetStudentQRCode(schoolStudentID);
-
-            if (qrcode == null)
-            {
-                return BadRequest();
-            }
-
-            return Ok(qrcode);
-        }
+        
     }
 }
