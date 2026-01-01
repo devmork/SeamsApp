ef5897c6bc3999756b283abde71afa201b293613
devmork <enoymarkden@gmail.com>
2026-01-01 23:22:23 +0800

    feat: add DTOs for student creation, update, and retrieval

---
+++ DTOs/Student/StudentCreationDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeamsApp.DTOs.Student
+{
+    public class StudentCreationDTO
+    {
+        [Required]
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        public string? SchoolStudentId { get; set; }
+        [Required]
+        public string? YearLevel { get; set; }
+        [Required]
+        public string? Course { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
---
+++ DTOs/Student/StudentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeamsApp.DTOs.Student
+{
+    public class StudentDTO
+    {
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? SchoolStudentId { get; set; }
+        public string? YearLevel { get; set; }
+        public string? Course { get; set; }
+        public string? Email { get; set; }
+        public byte[]? QRCode { get; set; }
+        public DateTime AddedAt { get; set; }
+    }
+}
