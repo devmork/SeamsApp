d04a52dd7d2308c12af34ce4e278713e808f66a0
devmork <enoymarkden@gmail.com>
2026-01-05 21:51:54 +0800

    feat: implement StudentService class to manage student operations

---
+++ Interfaces/Services/IStudentService.cs
@@ -0,0 +1,16 @@
+﻿using SeamsApp.DTOs.Student;
+using SeamsApp.Models.Base;
+
+namespace SeamsApp.Interfaces.Services
+{
+    public interface IStudentService
+    {
+        Task<IEnumerable<StudentDTO>> GetAllStudentAsync();
+        Task<int> AddStudentAsync(StudentCreationDTO studentCreationDTO);
+        Task<int> UpdateStudentByIdAsync(StudentUpdateDTO studentUpdateDTO, int studentId);
+        Task<int> DeleteStudentByIdAsync(int studentId);
+        Task<StudentDTO> GetStudentByIdAsync(int studentId);
+        Task<StudentDTO> GetStudentQRCodeAsync(string schoolStudentId);
+
+    }
+}
---
+++ Services/StudentService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using SeamsApp.DTOs.Student;
+using SeamsApp.Interfaces.Repositories;
+using SeamsApp.Interfaces.Services;
+using SeamsApp.Models.Base;
+
+namespace SeamsApp.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IStudentRepository _studentRepository;
+        private readonly IMapper _mapper;
+        public StudentService(IStudentRepository studentRepository, IMapper mapper)
+        {
+            _studentRepository = studentRepository;
+            _mapper = mapper;
+        }
+        public async Task<int> AddStudentAsync(StudentCreationDTO studentCreationDTO)
+        {
+            var student = _mapper.Map<Student>(studentCreationDTO);
+            return await _studentRepository.AddStudentAsync(student);
+        }
+        public async Task<int> DeleteStudentByIdAsync(int studentId)
+        {
+            var student = await _studentRepository.DeleteStudentByIdAsync(studentId);
+            return student;
+        }
+
+        public async Task<IEnumerable<StudentDTO>> GetAllStudentAsync()
+        {
+            var students = await _studentRepository.GetAllStudentAsync();
+            return _mapper.Map<IEnumerable<StudentDTO>>(students);
+        }
+
+        public async Task<StudentDTO> GetStudentByIdAsync(int studentId)
+        {
+            var student = await _studentRepository.GetStudentByIdAsync(studentId);
+            return _mapper.Map<StudentDTO>(student);
+        }
+
+        public Task<StudentDTO> GetStudentQRCodeAsync(string schoolStudentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateStudentByIdAsync(StudentUpdateDTO studentUpdateDTO, int studentId)
+        {
+            var product = _mapper.Map<Student>(studentUpdateDTO);
+            return _studentRepository.UpdateStudentByIdAsync(product);
+        }
+    }
+}
