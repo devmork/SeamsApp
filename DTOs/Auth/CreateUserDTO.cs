ee9c8d1d9446d85f4faa8aca77b26e94b8e62c51
devmork <enoymarkden@gmail.com>
2026-01-17 20:52:38 +0800

    feat: add CreateUserDTO class for user creation data transfer

---
+++ DTOs/Auth/CreateUserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeamsApp.DTOs.Auth
+{
+    public class CreateUserDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
