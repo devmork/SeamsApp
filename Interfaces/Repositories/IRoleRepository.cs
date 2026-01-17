dcf647742245dbdbec8de1accada47627e0b1382
devmork <enoymarkden@gmail.com>
2026-01-17 21:28:17 +0800

    feat: add IRoleRepository interface for role management

---
+++ Interfaces/Repositories/IRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using SeamsApp.Models;
+
+namespace SeamsApp.Interfaces.Repositories
+{
+    public interface IRoleRepository
+    {
+        Task<Role?> GetRoleByIdAsync(int roleId);
+        Task<IEnumerable<Role>> GetAllRolesAsync();
+    }
+}
