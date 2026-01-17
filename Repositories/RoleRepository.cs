0e4f77f706f0a0abdb9696ab65133e61a2c5a58b
devmork <enoymarkden@gmail.com>
2026-01-17 21:28:48 +0800

    feat: implement RoleRepository for role management functionality

---
+++ Repositories/RoleRepository.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using SeamsApp.Interfaces.Repositories;
+using SeamsApp.Models;
+
+namespace SeamsApp.Repositories
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly string _connectionString;
+        public RoleRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+
+        }
+        public async Task<IEnumerable<Role>> GetAllRolesAsync()
+        {
+            var sql = @"SELECT * FROM Roles";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var roles = await connection.QueryAsync<Role>(sql);
+                return roles;
+            }
+        }
+
+        public async Task<Role?> GetRoleByIdAsync(int roleId)
+        {
+            var sql = @"SELECT * FROM Roles WHERE RoleId = @RoleId";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var role = await connection.QueryFirstOrDefaultAsync<Role>(sql, new { RoleId = roleId });
+                return role!;
+            }
+        }
+    }
+}
