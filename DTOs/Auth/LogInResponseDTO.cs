3fe1b7b8b73158116a89dec53de4adae7ef0d924
devmork <enoymarkden@gmail.com>
2026-01-18 23:23:22 +0800

    fix: correct class name casing for LoginRequestDTO and LoginResponseDTO

--- DTOs/Auth/LogInResponseDTO.cs
+++ DTOs/Auth/LogInResponseDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace SeamsApp.DTOs.Auth
 {
-    public class LogInResponseDTO
+    public class LoginResponseDTO
     {
         public string Token { get; set; } = "";
         public UserDTO User { get; set; } = null!;
