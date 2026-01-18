48ce1a3a101ef7da5b0c2c49799eee840512fdef
devmork <enoymarkden@gmail.com>
2026-01-18 23:24:05 +0800

    feat: add IAuthService interface for user authentication operations

---
+++ Interfaces/Services/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using SeamsApp.DTOs.Auth;
+
+namespace SeamsApp.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<UserDTO> GetUserByEmailAsync(string email);
+        Task<UserDTO> GetUserByIdAsync(int userId);
+        Task<CreateUserDTO> CreateUserAsync(CreateUserDTO createUserDTO);
+        Task<IEnumerable<UserDTO>> GetAllUsersAsync();
+        Task<LoginResponseDTO> LoginAsync(string email, string password);
+    }
+}
