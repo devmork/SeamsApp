59ea5cc713034026091df6e9a7fddfbe431dfee3
devmork <enoymarkden@gmail.com>
2026-01-22 00:19:32 +0800

    fix: remove duplicate registration of IStudentService in Program.cs

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddScoped<IAttendanceRepository, AttendanceRepository>();
 
 // REGISTER SERVICES
 
-
+builder.Services.AddScoped<IStudentService, StudentService>();
 builder.Services.AddTransient<IAuthService, AuthService>();
 builder.Services.AddScoped<IJwtService, JwtService>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
