9cd421acf8fa708d12497a7a7e27b5363d45a1cb
devmork <enoymarkden@gmail.com>
2026-01-21 20:19:23 +0800

    fix: set default RoleId for UserRole to 2

--- Models/UserRole.cs
+++ Models/UserRole.cs
@@ -4,7 +4,7 @@
     {
         public int UserRoleId { get; set; }
         public int UserId { get; set; }
-        public int RoleId { get; set; }
+        public int RoleId { get; set; } = 2; // DEFAULT ROLE ID FOR 'User'
         public DateTime AssignedAt { get; set; } = DateTime.UtcNow;
     }
 }
