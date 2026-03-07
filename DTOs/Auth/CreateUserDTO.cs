6fa2f809f80d496b4d0081b38aa2e556a4e536b7
devmork <enoymarkden@gmail.com>
2026-03-07 23:15:20 +0800

    feat: add additional properties to CreateUserDTO for user details

--- DTOs/Auth/CreateUserDTO.cs
+++ DTOs/Auth/CreateUserDTO.cs
@@ -8,5 +8,12 @@ namespace SeamsApp.DTOs.Auth
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? SchoolStudentId { get; set; }
+        public string? YearLevel { get; set; }
+        public string? Course { get; set; }
+        public byte[]? QRCode { get; set; }
     }
 }
