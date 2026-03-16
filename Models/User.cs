37b7d5fe768348771503fada9770c48b48d38303
devmork <enoymarkden@gmail.com>
2026-03-16 09:42:03 +0800

    refactor(IUserRepository): comment out CreateUserAsync method for future implementation refactor(User): restore Email property for user model clarity

--- Models/User.cs
+++ Models/User.cs
@@ -12,6 +12,7 @@ namespace SeamsApp.Models
         [Key]
         public int UserId { get; set; }
         public int StudentId { get; set; }
+        public string? Email { get; set; }
         public string? PasswordHash { get; set; }
 
         [DataType(DataType.DateTime)]
