0f42e42bf904db86ae03b3dcacedbfb3f267597f
devmork <enoymarkden@gmail.com>
2026-03-16 14:38:30 +0800

    refactor(Program): remove unused RoleRepository and UserRoleRepository registrations

--- Program.cs
+++ Program.cs
@@ -20,8 +20,6 @@ SqlMapper.AddTypeHandler(new TimeOnlyHandler());
 // REGISTER REPOSITORIES
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IRoleRepository, RoleRepository>();
-builder.Services.AddScoped<IUserRoleRepository, UserRoleRepository>();
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 builder.Services.AddScoped<IAttendanceRepository, AttendanceRepository>();
 
