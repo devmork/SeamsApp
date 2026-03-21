d48d73fa1e2226e0e9b9990c78ddb350407813d7
devmork <enoymarkden@gmail.com>
2026-03-21 13:13:37 +0800

    feat(AdminDTO): add CreateAdminDTO and remove RegisterUserDTO

---
+++ DTOs/User/CreateAdminDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeamsApp.DTOs.Auth
+{
+    public class CreateAdminDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string PasswordHash { get; set; } = null!;
+    }
+}
