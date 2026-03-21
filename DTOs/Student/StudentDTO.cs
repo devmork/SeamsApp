77729a94f60376759837798cf371267146e38650
devmork <enoymarkden@gmail.com>
2026-03-21 21:20:02 +0800

    refactor(StudentDTO): reorganize properties for clarity and consistency

--- DTOs/Student/StudentDTO.cs
+++ DTOs/Student/StudentDTO.cs
@@ -4,13 +4,21 @@ namespace SeamsApp.DTOs.Student
 {
     public class StudentDTO
     {
+       // PERSONAL INFORMATION
+        public int StudentId { get; set; }
         public string? FirstName { get; set; }
         public string? MiddleName { get; set; }
         public string? LastName { get; set; }
-        public string? SchoolStudentId { get; set; }
-        public string? YearLevel { get; set; }
-        public string? Course { get; set; }
+        public string? Suffix { get; set; }
         public string? Email { get; set; }
+
+        // SCHOOL INFORMATION
+        public string? SchoolStudentId { get; set; }
+        public int YearLevel { get; set; }
+        public string? Course { get; set; }
+
+        // PHOTO URL
+        public string? PhotoUrl { get; set; }
         public byte[]? QRCode { get; set; }
     }
 }
