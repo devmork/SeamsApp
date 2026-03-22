a755616a3de4f8191394e2451176dfe18574e049
GiroGodito <goditolitoarthon@gmail.com>
2026-03-22 19:07:26 +0800

    feat: added cross-platform qr code generator using SkiaSharp.QrCode nuget package, but still retains original code for rollback purposes (undo processes)

--- Utilities/QRCodeUtility.cs
+++ Utilities/QRCodeUtility.cs
@@ -1,15 +1,49 @@
-﻿using System.Drawing;
+﻿// using System.Drawing;
+// using System.IO;
+// using System.Drawing.Imaging;
+// using QRCoder;
+// using static QRCoder.QRCodeGenerator;
+// using System.Runtime.Versioning;
+
+// namespace SeamsApp.Utilities
+// {
+//     public class QRCodeUtility
+//     {
+//         [SupportedOSPlatform("windows")]
+//         public static byte[] GenerateQRCode(
+//             string firstName, 
+//             string middleName, 
+//             string lastName, 
+//             string schoolStudentId)
+//         {
+//             string qrContent = $"{firstName} {middleName} {lastName} - {schoolStudentId}";
+
+//             var qrGenerator = new QRCoder.QRCodeGenerator();
+//             var qrCodeData = qrGenerator.CreateQrCode(qrContent, QRCoder.QRCodeGenerator.ECCLevel.Q);
+
+
+//             using (var qrCode = new QRCode(qrCodeData))
+//             {
+//                 using (var bitmap = qrCode.GetGraphic(20))
+//                 {
+//                     using (var memoryStream = new MemoryStream())
+//                     {
+//                         bitmap.Save(memoryStream, ImageFormat.Png);
+//                         return memoryStream.ToArray();
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
 using System.IO;
-using System.Drawing.Imaging;
-using QRCoder;
-using static QRCoder.QRCodeGenerator;
-using System.Runtime.Versioning;
+using SkiaSharp.QrCode.Image; // Use the correct namespace
 
 namespace SeamsApp.Utilities
 {
     public class QRCodeUtility
     {
-        [SupportedOSPlatform("windows")]
         public static byte[] GenerateQRCode(
             string firstName, 
             string middleName, 
@@ -18,21 +52,9 @@ namespace SeamsApp.Utilities
         {
             string qrContent = $"{firstName} {middleName} {lastName} - {schoolStudentId}";
 
-            var qrGenerator = new QRCoder.QRCodeGenerator();
-            var qrCodeData = qrGenerator.CreateQrCode(qrContent, QRCoder.QRCodeGenerator.ECCLevel.Q);
-
-
-            using (var qrCode = new QRCode(qrCodeData))
-            {
-                using (var bitmap = qrCode.GetGraphic(20))
-                {
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        bitmap.Save(memoryStream, ImageFormat.Png);
-                        return memoryStream.ToArray();
-                    }
-                }
-            }
+            // This one-liner handles everything for you.
+            // It generates the QR code and returns the raw PNG bytes.
+            return QRCodeImageBuilder.GetPngBytes(qrContent);
         }
     }
 }
