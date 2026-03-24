54cdb68ee039452213e25e054251dcd993fe2f11
devmork <enoymarkden@gmail.com>
2026-03-24 16:39:06 +0800

    feat(IOfficerRepository): add methods to retrieve and update officer statuses

--- Interfaces/Repositories/IOfficerRepository.cs
+++ Interfaces/Repositories/IOfficerRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace SeamsApp.Interfaces.Repositories
+﻿using SeamsApp.DTOs.Officer;
+using SeamsApp.Models.Base;
+
+namespace SeamsApp.Interfaces.Repositories
 {
     public interface IOfficerRepository
     {
+        Task<IEnumerable<OfficerDTO>> GetAllOfficersAsync();
+        Task<int> UpdateStudentStatusToOfficer(int studentId, int status);
+        Task<int> RemoveOfficerAsync(int studentId, int status);
     }
 }
