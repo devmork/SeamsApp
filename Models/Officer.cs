ebd9efcf8b4b87f2bac050e65f5dca8b165d0985
devmork <enoymarkden@gmail.com>
2026-06-22 21:34:46 +0800

    feat(api): add Officer model with user and audit fields

---
+++ Models/Officer.cs
@@ -0,0 +1,10 @@
+﻿namespace SeamsApp.Models
+{
+    public class Officer
+    {
+        public int OfficerId { get; set; }
+        public int UserId { get; set; }
+        public int AddedBy { get; set; }
+        public DateTime AddedAt { get; set; } = DateTime.UtcNow;
+    }
+}
