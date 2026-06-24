968ce31d3b2ea08b5f40347d4dc48bad2d4ffb77
devmork <enoymarkden@gmail.com>
2026-06-25 00:55:04 +0800

    chore(api): add EF Core migration to update Event model properties

---
+++ Migrations/20260624165223_UpdateEventModelProperties.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeamsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEventModelProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "LogType",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Events");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Events");
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "EndTime",
+                table: "Events",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogType",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "StartTime",
+                table: "Events",
+                type: "time",
+                nullable: true);
+        }
+    }
+}
