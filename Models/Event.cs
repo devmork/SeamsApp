ecac81c62112e6f0e466bd374d8be0ef8dfee956
devmork <enoymarkden@gmail.com>
2026-06-25 00:54:18 +0800

    refactor(api): update Event model by removing time fields and adding Status

--- Models/Event.cs
+++ Models/Event.cs
@@ -7,9 +7,7 @@ namespace SeamsApp.Models
         [Key]
         public int EventId { get; set; }
         public string? Title { get; set; }
-        public string? LogType { get; set; }
-        public TimeOnly? StartTime { get; set; }
-        public TimeOnly EndTime { get; set; }
+        public int Status { get; set; }
         public int CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
