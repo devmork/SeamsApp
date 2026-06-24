b737a8c941464c0ac1af14b4c62e2d1554a9fea8
devmork <enoymarkden@gmail.com>
2026-06-24 22:11:06 +0800

    refactor: change YearLevel data type to int and remove Email property

--- Models/Student.cs
+++ Models/Student.cs
@@ -17,11 +17,10 @@ namespace SeamsApp.Models.Base
         public string? MiddleName { get; set; }
         public string? LastName { get; set; }
         public string? Suffix { get; set; }
-        public string? Email { get; set; }
 
         // SCHOOL INFORMATION
         public string? SchoolStudentId { get; set; }
-        public int YearLevel { get; set; }
+        public string? YearLevel { get; set; }
         public string? Course { get; set; }
 
         // PHOTO URL
