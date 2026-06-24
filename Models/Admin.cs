122f5585ee2500cd8962c1b8fb6df8ff5b003724
devmork <enoymarkden@gmail.com>
2026-06-24 22:58:37 +0800

    feat: add CreatedAt property to Admin model

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -6,6 +6,7 @@ namespace SeamsApp.Models
     {
         [Key]
         public int AdminId { get; set; }
-        public int UserId { get; set; }        
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
