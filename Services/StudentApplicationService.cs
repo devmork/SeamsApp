0d755aa96634bdfb0597b11d3e41752f239b9f84
devmork <enoymarkden@gmail.com>
2026-06-24 22:05:38 +0800

    feat: implement IStudentApplications interface methods

--- Services/StudentApplicationService.cs
+++ Services/StudentApplicationService.cs
@@ -1,9 +1,14 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using QRCoder;
 using SeamsApp.Data;
 using SeamsApp.DTOs.StudentApplication;
 using SeamsApp.Interfaces.Services;
 using SeamsApp.Models;
+using SeamsApp.Models.Base;
+using SeamsApp.Utilities;
 
 namespace SeamsApp.Services
 {
@@ -11,10 +16,19 @@ namespace SeamsApp.Services
     {
         private readonly IMapper _mapper;
         private readonly SeamsDbContext _dbContext;
-        public StudentApplicationService(IMapper mapper,SeamsDbContext dbContext)
+        private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public StudentApplicationService(
+             IMapper mapper,
+             SeamsDbContext dbContext,
+             IPasswordHasher<User> passwordHasher,
+             IHttpContextAccessor httpContextAccessor)
         {
             _dbContext = dbContext;
             _mapper = mapper;
+            _passwordHasher = passwordHasher;
+            _httpContextAccessor = httpContextAccessor;
         }
         public async Task<CreateStudentApplicationRequest> CreateStudentApplication(CreateStudentApplicationRequest createStudentApplicationRequest)
         {
@@ -34,12 +48,49 @@ namespace SeamsApp.Services
                 return 0;
             }
 
+            //var userId = ClaimsUtility.GetUserIdFromClaims(_httpContextAccessor.HttpContext!);
+
+            var user = new User
+            {
+                Email = existingStudentApplication.Email,
+                Role = "Student",
+                IsActive = 1,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            user.PasswordHash = _passwordHasher.HashPassword(user, existingStudentApplication.LastName!.ToUpper());
+
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+
+            var student = new Student
+            {
+                UserId = user.UserId,
+                FirstName = existingStudentApplication.FirstName,
+                MiddleName = existingStudentApplication.MiddleName,
+                LastName = existingStudentApplication.LastName,
+                Suffix = existingStudentApplication.Suffix,
+                SchoolStudentId = existingStudentApplication.SchoolStudentId,
+                YearLevel = existingStudentApplication.YearLevel,
+                Course = existingStudentApplication.Course,
+                PhotoUrl = existingStudentApplication.PhotoUrl,
+                QRCode = QRCodeUtility.GenerateQRCode(existingStudentApplication.FirstName!, 
+                                                      existingStudentApplication.MiddleName!, 
+                                                      existingStudentApplication.LastName, 
+                                                      existingStudentApplication.Suffix,
+                                                      existingStudentApplication.SchoolStudentId!)
+            };
+
+            _dbContext.Students.Add(student);
+
             existingStudentApplication.Status = 2; // APPROVED
+            //existingStudentApplication.ReviewedBy = userId;
+            existingStudentApplication.ReviewedAt = DateTime.UtcNow;
             _dbContext.StudentApplications.Update(existingStudentApplication);
             return await _dbContext.SaveChangesAsync();
 
         }
-        public async Task<int> RejectStundetApplication(int studentApplicationId)
+        public async Task<int> RejectStudentApplication(int studentApplicationId)
         {
             var existingStudentApplication = await _dbContext.StudentApplications.FindAsync(studentApplicationId);
             if (existingStudentApplication == null)
@@ -51,19 +102,31 @@ namespace SeamsApp.Services
             _dbContext.StudentApplications.Update(existingStudentApplication);
             return await _dbContext.SaveChangesAsync();
         }
-        public Task<IEnumerable<StudentApplicationResponse>> GetAllApproveStudentApplicationsAsync()
+        public async Task<IEnumerable<StudentApplicationResponse>> GetAllPendingStudentApplicationsAsync()
         {
-            throw new NotImplementedException();
+            var approvedApplications = await _dbContext.StudentApplications.Where(app => app.Status == 1).ToListAsync();
+            var response = _mapper.Map<IEnumerable<StudentApplicationResponse>>(approvedApplications);
+            return response;
+        }
+        public async Task<IEnumerable<StudentApplicationResponse>> GetAllApprovedStudentApplicationsAsync()
+        {
+            var approvedApplications = await _dbContext.StudentApplications.Where(app => app.Status == 2).ToListAsync();
+            var response = _mapper.Map<IEnumerable<StudentApplicationResponse>>(approvedApplications);
+            return response;
         }
 
-        public Task<IEnumerable<StudentApplicationResponse>> GetAllPendingStudentApplicationsAsync()
+        public async Task<IEnumerable<StudentApplicationResponse>> GetAllRejectedStudentApplicationsAsync()
         {
-            throw new NotImplementedException();
+            var rejectApplications = await _dbContext.StudentApplications.Where(app => app.Status == 3).ToListAsync();
+            var response = _mapper.Map<IEnumerable<StudentApplicationResponse>>(rejectApplications);
+            return response;
         }
 
-        public Task<IEnumerable<StudentApplicationResponse>> GetAllStudentApplicationsAsync()
+        public async Task<IEnumerable<StudentApplicationResponse>> GetAllStudentApplicationsAsync()
         {
-            throw new NotImplementedException();
+            var studentApplications = await _dbContext.StudentApplications.ToListAsync();
+            var response = _mapper.Map<IEnumerable<StudentApplicationResponse>>(studentApplications);
+            return response;
         }
 
         
