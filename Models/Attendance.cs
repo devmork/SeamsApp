8788a2ea86ac29e7524a21060bc04daced42663f
devmork <enoymarkden@gmail.com>
2026-06-25 14:04:08 +0800

    refactor(api): update Attendance model by removing Semester and adjusting Status

--- Models/Attendance.cs
+++ Models/Attendance.cs
@@ -15,12 +15,11 @@ namespace SeamsApp.Models.Base
         public string? Title { get; set; }
         public DateTime Date { get; set; }
         public string? LogType { get; set; }  
-        public int Semester { get; set; }
         [Required]
         public DateTime StartTime { get; set; }
         [Required]
         public DateTime EndTime { get; set; }
-        public int Status { get; set; } = 1;
+        public int Status { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
