a0238321e3c2f103526764082af33af207b671f0
devmork <enoymarkden@gmail.com>
2026-06-26 04:02:37 +0800

    refactor(api): remove obsolete Attendance DTO files

--- DTOs/Attendance/AttendanceRecordDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace SeamsApp.DTOs.Attendance
-{
-    public class AttendanceRecordDTO
-    {
-        public string FullName {get;set;} = null!;
-        public string SchoolStudentId {get;set;} = null!;
-    }
-}
