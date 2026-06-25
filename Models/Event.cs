2eb3a866389516a525c00a4107943d53f3352e87
devmork <enoymarkden@gmail.com>
2026-06-26 01:54:17 +0800

    refactor(api): update Event model by adding StartDate and EndDate

--- Models/Event.cs
+++ Models/Event.cs
@@ -7,6 +7,8 @@ namespace SeamsApp.Models
         [Key]
         public int EventId { get; set; }
         public string? Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public int Status { get; set; }
         public int CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
