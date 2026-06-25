e60e4e8502e52ea41c858cdac3d9128a621797b9
devmork <enoymarkden@gmail.com>
2026-06-26 03:45:29 +0800

    chore(api): add EF Core migrations for new Attendance properties

---
+++ Migrations/20260625180638_AddNewAttendanceProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeamsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewAttendanceProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Session",
+                table: "Attendances",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Session",
+                table: "Attendances");
+        }
+    }
+}
---
+++ Migrations/20260625185659_AddedNewAttendanceProperties.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeamsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNewAttendanceProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "Attendances",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Attendances",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Attendances");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Attendances");
+        }
+    }
+}
