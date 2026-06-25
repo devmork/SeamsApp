62418709a7ee398e81caae8cf333bb64abbd1ac1
devmork <enoymarkden@gmail.com>
2026-06-25 14:06:56 +0800

    chore(api): add EF Core migration to update Attendance model properties

--- Migrations/20260625060308_UpdateAttendanceModelProperties.Designer.cs
+++ Migrations/20260625060308_UpdateAttendanceModelProperties.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SeamsApp.Data;
 
@@ -11,9 +12,11 @@ using SeamsApp.Data;
 namespace SeamsApp.Migrations
 {
     [DbContext(typeof(SeamsDbContext))]
-    partial class SeamsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260625060308_UpdateAttendanceModelProperties")]
+    partial class UpdateAttendanceModelProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
