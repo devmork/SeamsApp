6ad9376406c81be1b0013f15288eed8183661013
devmork <enoymarkden@gmail.com>
2026-06-26 22:15:29 +0800

    refactor: remove unused AttendanceRecordDTO

--- DTOs/AttendanceRecords/AttendanceRecordDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace SeamsApp.DTOs.AttendanceRecords
-{
-    public class AttendanceRecordDTO
-    {
-        public string FullName {get;set;} = null!;
-        public string SchoolStudentId {get;set;} = null!;
-    }
-}
