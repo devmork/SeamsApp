1e21a7653a235ce24967343b18ef7574a3147897
devmork <enoymarkden@gmail.com>
2026-06-26 21:24:08 +0800

    chore(api): add EF Core migration for new Officer property

---
+++ Migrations/20260625211343_AddedNewOfficerProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeamsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNewOfficerProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Officers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Officers");
+        }
+    }
+}
