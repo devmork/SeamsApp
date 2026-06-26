b93eba39d804cdebcf6a7d7207448a24290bf396
devmork <enoymarkden@gmail.com>
2026-06-26 22:17:05 +0800

    feat(api): add Officer request and response DTOs

---
+++ DTOs/Officer/OfficerResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace SeamsApp.DTOs.Officer
+{
+    public class OfficerResponse
+    {
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Suffix { get; set; }
+        public string? SchoolStudentId { get; set; }
+        public string? YearLevel { get; set; }
+        public string? Course { get; set; }
+        public string? Position { get; set; }
+        public string? PhotoUrl { get; set; }
+    }
+}
