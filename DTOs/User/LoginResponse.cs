a517f1bc0e4ae776633ec5f5cce83090de8609c5
devmork <enoymarkden@gmail.com>
2026-06-30 02:51:59 +0800

    refactor(backend): rename LoginRequestDTO and LoginResponseDTO to LoginRequest and LoginResponse

--- DTOs/User/LoginResponse.cs
+++ DTOs/User/LoginResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace SeamsApp.DTOs.Auth
 {
-    public class LoginResponseDTO
+    public class LoginResponse
     {
         public string Token { get; set; } = "";
         public UserDTO User { get; set; } = null!;
