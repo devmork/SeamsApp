b8169e9f455b910c6a021719dffe14e9cb53cead
devmork <enoymarkden@gmail.com>
2026-06-30 13:17:31 +0800

    refactor(backend): move IUserService to Commands and update LoginAsync return type

--- Interfaces/Services/Commands/IUserService.cs
+++ Interfaces/Services/Commands/IUserService.cs
@@ -1,14 +1,14 @@
 ﻿using SeamsApp.DTOs.Auth;
 using SeamsApp.DTOs.Student;
 
-namespace SeamsApp.Interfaces.Services.Queries
+namespace SeamsApp.Interfaces.Services.Commands
 {
     public interface IUserService
     {
         Task<UserDTO> GetUserByEmailAsync(string email);
         Task<UserDTO> GetUserByIdAsync(int userId);
         Task<IEnumerable<UserDTO>> GetAllUsersAsync();
-        Task<LoginResponseDTO> LoginAsync(string email, string password);
+        Task<LoginResponse> LoginAsync(string email, string password);
         Task<CreateAdminDTO> CreateUserAsync(CreateAdminDTO createAdminDTO);
     }
 }
