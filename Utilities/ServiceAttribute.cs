334d12e99e1c25e5c7065163d7357bc563ae5504
devmork <enoymarkden@gmail.com>
2026-07-13 02:25:29 +0800

    feat(backend): add custom DI service lifetime attributes

---
+++ Utilities/ServiceAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace SeamsApp.Utilities
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class TransientServiceAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class ScopedServiceAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class SingletonServiceAttribute : Attribute { }
+}
