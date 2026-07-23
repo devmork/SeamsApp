33979257ff24438d51eef4e71e2c4c496c7b0d8e
devmork <enoymarkden@gmail.com>
2026-07-23 13:27:18 +0800

    chore(backend): add migrations for Officer and Student model updates

---
+++ Migrations/20260721080256_RemovePositionPropertyOnOfficerModel.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeamsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePositionPropertyOnOfficerModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Officers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_UserId",
+                table: "Students",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Officers_UserId",
+                table: "Officers",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admins_UserId",
+                table: "Admins",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Admins_Users_UserId",
+                table: "Admins",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Officers_Users_UserId",
+                table: "Officers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Users_UserId",
+                table: "Students",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Admins_Users_UserId",
+                table: "Admins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Officers_Users_UserId",
+                table: "Officers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Users_UserId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_UserId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Officers_UserId",
+                table: "Officers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Admins_UserId",
+                table: "Admins");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Officers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ Migrations/20260723052154_AddNavigationPropertyInModels.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeamsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNavigationPropertyInModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendances_EventId",
+                table: "Attendances",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttendanceRecords_AttendanceID",
+                table: "AttendanceRecords",
+                column: "AttendanceID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AttendanceRecords_Attendances_AttendanceID",
+                table: "AttendanceRecords",
+                column: "AttendanceID",
+                principalTable: "Attendances",
+                principalColumn: "AttendanceId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendances_Events_EventId",
+                table: "Attendances",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "EventId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AttendanceRecords_Attendances_AttendanceID",
+                table: "AttendanceRecords");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendances_Events_EventId",
+                table: "Attendances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Attendances_EventId",
+                table: "Attendances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AttendanceRecords_AttendanceID",
+                table: "AttendanceRecords");
+        }
+    }
+}
