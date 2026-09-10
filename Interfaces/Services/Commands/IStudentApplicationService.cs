8bdc26bcdfe97496a47008f6ecdd115d5cdedfc2
devmork <enoymarkden@gmail.com>
2026-09-11 00:49:23 +0800

    fix: correct typo in ApproveStudentApplication method name

--- Interfaces/Services/Commands/IStudentApplicationService.cs
+++ Interfaces/Services/Commands/IStudentApplicationService.cs
@@ -5,7 +5,7 @@ namespace SeamsApp.Interfaces.Services.Commands
     public interface IStudentApplicationService
     {
         Task<CreateStudentApplicationRequest> CreateStudentApplication(CreateStudentApplicationRequest createStudentApplicationRequest);
-        Task<int> ApproveStundetApplication(int studentApplicationId);
+        Task<int> ApproveStudentApplication(int studentApplicationId);
         Task<int> RejectStudentApplication(int studentApplicationId);      
         Task<IEnumerable<StudentApplicationResponse>> GetAllStudentApplicationsAsync();
         Task<IEnumerable<StudentApplicationResponse>> GetAllPendingStudentApplicationsAsync();
